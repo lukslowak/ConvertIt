417e49d1c3736eafcc02cc3bdbc7a6efae07ea59
lukslowak <lukslowak@gmail.com>
2020-03-19 21:12:59 +0100

    new page mathOperations.html, + - / * - complete

--- src/main/java/lukasz/slowak/CovertIt/HomeController.java
+++ src/main/java/lukasz/slowak/CovertIt/HomeController.java
@@ -11,4 +11,9 @@ public class HomeController {
         return "home";
     }
 
+    @GetMapping("/mathOperations")
+    public String mathOperations(){
+        return "mathOperations";
+    }
+
 }
