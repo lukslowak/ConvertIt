e0b4818137ded31b32b4171d3640426e104d8dc9
lukslowak <lukslowak@gmail.com>
2020-03-19 20:54:21 +0100

    second commit

---
+++ src/main/java/lukasz/slowak/CovertIt/CovertItApplication.java
@@ -0,0 +1,13 @@
+package lukasz.slowak.CovertIt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovertItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CovertItApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lukasz/slowak/CovertIt/CovertItApplicationTests.java
@@ -0,0 +1,13 @@
+package lukasz.slowak.CovertIt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovertItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
